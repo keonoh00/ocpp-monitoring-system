c58f6626bbc32efa0cd615ef3eaa7905cf98c538
keonoh00 <keonoh00@gmail.com>
2023-10-26 16:43:14 +0900

    Setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,6 @@ function App() {
         />
         <LongContainer title={"Server"} />
       </RowContainer>
-
       <StatusContainer />
     </AppContainer>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient();
 
