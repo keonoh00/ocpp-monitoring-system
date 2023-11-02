82b2dc769111c4e5db53867aba3f539376158c3d
keonoh00 <keonoh00@gmail.com>
2023-11-02 16:23:54 +0900

    Remove Material UI

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,10 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import client from "./api/apolloClient";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
