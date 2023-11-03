53970ac0cb3ee6df187fb5801879d9e38fba8ce3
keonoh00 <keonoh00@gmail.com>
2023-11-03 17:45:48 +0900

    Enable CORS for all origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
